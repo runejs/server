32d092c63ed141bc157be560335bf70b76d05439
TheBlackParade <runejs.dev@gmail.com>
2020-01-23 18:37:48 -0600

    Implementing truly horribly coded gates.

--- src/world/mob/player/packet/impl/object-interaction-packet.ts
+++ src/world/mob/player/packet/impl/object-interaction-packet.ts
@@ -6,9 +6,11 @@ import { world } from '@server/game-server';
 import { doorAction } from '@server/world/mob/player/action/doors/door-action';
 import { walkToAction } from '@server/world/mob/player/action/action';
 import { doubleDoorAction } from '@server/world/mob/player/action/doors/double-door-action';
+import { gateAction } from '@server/world/mob/player/action/doors/gate-action';
 
 const doors = [1530, 4465, 4467, 3014, 3017, 3018, 3019, 1536, 1533, 1531, 1534, 12348];
 const doubleDoors = [12349, 12350, 1519, 1516];
+const gates = [1551, 1553];
 
 export const objectInteractionPacket: incomingPacket = (player: Player, packetId: number, packetSize: number, packet: RsBuffer): void => {
     const x = packet.readNegativeOffsetShortBE();
@@ -37,6 +39,8 @@ export const objectInteractionPacket: incomingPacket = (player: Player, packetId
         walkToAction(player, objectPosition).then(() => doorAction(player, chunkObject, objectPosition, cacheOriginal));
     } else if(doubleDoors.indexOf(objectId) !== -1) {
         walkToAction(player, objectPosition).then(() => doubleDoorAction(player, chunkObject, objectPosition, cacheOriginal));
+    } else if(gates.indexOf(objectId) !== -1) {
+        walkToAction(player, objectPosition).then(() => gateAction(player, chunkObject, objectPosition, cacheOriginal));
     } else {
         player.packetSender.chatboxMessage(`Unhandled object interaction: ${objectId} @ ${x},${y}`);
     }
