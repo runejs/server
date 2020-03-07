078914b71bdbf2e2f0dd8b5d1a595a25a42bd048
TheBlackParade <runejs.dev@gmail.com>
2020-03-06 22:40:36 -0600

    Running some test implementations of the new dialogue system.

--- src/plugins/quests/cooks-assistant-quest.ts
+++ src/plugins/quests/cooks-assistant-quest.ts
@@ -2,96 +2,76 @@ import { npcAction } from '@server/world/actor/player/action/npc-action';
 import { ActionType, RunePlugin } from '@server/plugins/plugin';
 import { npcIds } from '@server/world/config/npc-ids';
 import { quests } from '@server/world/config/quests';
-import { dialogueAction, DialogueEmote } from '@server/world/actor/player/action/dialogue-action';
+import { dialogue, Emote, execute } from '@server/world/actor/dialogue';
 
 const startQuestAction: npcAction = (details) => {
     const { player, npc } = details;
 
-    dialogueAction(player)
-        .then(d => d.npc(npc, DialogueEmote.DISTRESSED_1, [ 'What am I to do?' ]))
-        .then(d => d.options('Select an Option', [ `What's wrong?`, `Can you make me a cake?`, `You don't look very happy.`, `Nice hat!` ]))
-        .then(d => {
-            switch(d.action) {
-                case 1:
-                    return d.player(DialogueEmote.DEFAULT, [ `What's wrong?` ]);
-                case 2:
-                    return d.player(DialogueEmote.DEFAULT, [ `You're a cook, why don't you bake me a cake?` ])
-                        .then(d => d.npc(npc, DialogueEmote.SAD_1, [ `*sniff* Don't talk to me about cakes...` ]))
-                        .then(d => d.player(DialogueEmote.DEFAULT, [ `What's wrong?` ]));
-                case 3:
-                    return d.player(DialogueEmote.DEFAULT, [ `You don't look very happy.` ])
-                        .then(d => d.npc(npc, DialogueEmote.SAD_1, [ `No, I'm not. The world is caving in around me - I am`,
-                            `overcome by dark feelings of impending doom.` ]))
-                        .then(d => d.options('Select an Option', [ `What's wrong?`, `I'd take the rest of the day off if I were you.` ]))
-                        .then(d => {
-                            switch(d.action) {
-                                case 1:
-                                    return d.player(DialogueEmote.DEFAULT, [ `What's wrong?` ]);
-                                case 2:
-                                    return d.player(DialogueEmote.DEFAULT, [ `I'd take the rest of the day off if I were you.` ])
-                                        .then(d => d.npc(npc, DialogueEmote.CALM_TALK_1, [ `No, that's the worst thing I could do. I'd get in terrible`,
-                                            `trouble.` ]))
-                                        .then(d => d.player(DialogueEmote.CONSIDERING, [ `Well maybe you need to take a holiday...` ]))
-                                        .then(d => d.npc(npc, DialogueEmote.CALM_TALK_1, [ `That would be nice, but the Duke doesn't allow holidays`,
-                                            `for core staff.` ]))
-                                        .then(d => d.player(DialogueEmote.CONSIDERING, [ `Hmm, why not run away to the sea and start a new`,
-                                            `life as a Pirate?` ]))
-                                        .then(d => d.npc(npc, DialogueEmote.SAD_2, [ `My wife gets sea sick, and I have an irrational fear of`,
-                                            `eyepatches. I don't see it working myself.` ]))
-                                        .then(d => d.player(DialogueEmote.DEFAULT, [ `I'm afraid I've run out of ideas.` ]))
-                                        .then(d => d.npc(npc, DialogueEmote.SAD_1, [ `I know I'm doomed.` ]))
-                                        .then(d => d.player(DialogueEmote.DEFAULT, [ `What's wrong?` ]));
-                            }
-                        });
-                case 4:
-                    return d.player(DialogueEmote.DEFAULT, [ `Nice hat!` ])
-                        .then(d => d.npc(npc, DialogueEmote.DEFAULT, [ `Err thank you. It's a pretty ordinary cooks hat really.` ]))
-                        .then(d => d.player(DialogueEmote.CONSIDERING, [ `Still, suits you. The trousers are pretty special too.` ]))
-                        .then(d => d.npc(npc, DialogueEmote.DEFAULT, [ `It's all standard cook's issue uniform...` ]))
-                        .then(d => d.player(DialogueEmote.CONSIDERING, [ `The whole hat, apron, striped trousers ensemble - it`,
-                            `works. It makes you look like a real cook.` ]))
-                        .then(d => d.npc(npc, DialogueEmote.ANGRY_1, [ `I am a real cook! I haven't got time to be chatting`,
-                            `about Culinary Fashion. I am in desperate need of help!` ]))
-                        .then(d => d.player(DialogueEmote.DEFAULT, [ `What's wrong?` ]));
-            }
+    dialogue([ player, { npc, key: 'cook' }], [
+        cook => [ Emote.WORRIED, `What am I to do?` ],
+        () => ({
+            'What\'s wrong?': [],
+            'Can you make me a cake?': [
+                player => [ Emote.HAPPY, `You're a cook, why don't you bake me a cake?` ],
+                cook => [ Emote.SAD, `*sniff* Don't talk to me about cakes...` ]
+            ],
+            'You don\'t look very happy.': [
+                player => [ Emote.WORRIED, `You don't look very happy.` ],
+                cook => [ Emote.SAD, `No, I'm not. The world is caving in around me - I am overcome by dark feelings of impending doom.` ],
+                () => ({
+                    'What\'s wrong?': [],
+                    'I\'d take the rest of the day off if I were you.': [
+                        player => [ Emote.GENERIC, `I'd take the rest of the day off if I were you.` ],
+                        cook => [ Emote.WORRIED, `No, that's the worst thing I could do. I'd get in terrible trouble.` ],
+                        player => [ Emote.SKEPTICAL, `Well maybe you need to take a holiday...` ],
+                        cook => [ Emote.SAD, `That would be nice, but the Duke doesn't allow holidays for core staff.` ],
+                        player => [ Emote.LAUGH, `Hmm, why not run away to the sea and start a new life as a Pirate?` ],
+                        cook => [ Emote.SKEPTICAL, `My wife gets sea sick, and I have an irrational fear of eyepatches. I don't see it working myself.` ],
+                        player => [ Emote.WORRIED, `I'm afraid I've run out of ideas.` ],
+                        cook => [ Emote.SAD, `I know I'm doomed.` ]
+                    ]
+                })
+            ],
+            'Nice hat!': [
+                player => [ Emote.HAPPY, `Nice hat!` ],
+                cook => [ Emote.SKEPTICAL, `Err thank you. It's a pretty ordinary cooks hat really.` ],
+                player => [ Emote.HAPPY, `Still, suits you. The trousers are pretty special too.` ],
+                cook => [ Emote.SKEPTICAL, `It's all standard cook's issue uniform...` ],
+                player => [ Emote.POMPOUS, `The whole hat, apron, striped trousers ensemble - it works. It makes you look like a real cook.` ],
+                cook => [ Emote.ANGRY, `I am a real cook! I haven't got time to be chatting about Culinary Fashion. I am in desperate need of help!` ]
+            ]
+        }),
+        player => [ Emote.HAPPY, `What's wrong?` ],
+        cook => [ Emote.WORRIED, `Oh dear, oh dear, oh dear, I'm in a terrible terrible ` +
+            ` mess! It's the Duke's birthday today, and I should be making him a lovely big birthday cake.` ],
+        cook => [ Emote.WORRIED, `I've forgotten to buy the ingredients. I'll never get ` +
+            `them in time now. He'll sack me! What will I do? I have four children and a goat to ` +
+            `look after. Would you help me? Please?` ],
+        () => ({
+            'I\'m always happy to help a cook in distress.': [
+                execute(() => {
+
+                }),
+                player => [ Emote.GENERIC, `Yes, I'll help you.` ],
+                cook => [ Emote.HAPPY, `Oh thank you, thank you. I need milk, an egg and flour. I'd be very grateful ` +
+                    `if you can get them for me.` ],
+                player => [ Emote.GENERIC, `So where do I find these ingredients then?` ]
+            ],
+            'I can\'t right now, maybe later.': [
+                player => [ Emote.GENERIC, `No, I don't feel like it. Maybe later.` ],
+                cook => [ Emote.ANGRY, `Fine. I always knew you Adventurer types were callous beasts. Go on your merry way!` ]
+                // @TODO options for "where do I find x"
+            ]
         })
-        .then(d => d.npc(npc, DialogueEmote.DISTRESSED_1, [ `Oh dear, oh dear, oh dear, I'm in a terrible terrible`,
-            `mess! It's the Duke's birthday today, and I should be`, `making him a lovely big birthday cake.` ]))
-        .then(d => d.npc(npc, DialogueEmote.DISTRESSED_2, [ `I've forgotten to buy the ingredients. I'll never get`,
-            `them in time now. He'll sack me! What will I do? I have`, `four children and a goat to look after. Would you help`,
-            `me? Please?` ]))
-        .then(d => d.options('Select an Option', [ `I'm always happy to help a cook in distress.`, `I can't right now, maybe later.` ]))
-        .then(d => {
-            switch(d.action) {
-                case 1:
-                    player.setQuestStage(quests.cooksAssistant, 'COLLECTING');
-                    return d.player(DialogueEmote.DEFAULT, [ `Yes, I'll help you.` ]) // @TODO set stage to started
-                        .then(d => d.npc(npc, DialogueEmote.DEFAULT, [ `Oh thank you, thank you. I need milk, an egg and`,
-                            `flour. I'd be very grateful if you can get them for me.` ]))
-                        .then(d => d.player(DialogueEmote.DEFAULT, [ `So where do I find these ingredients then?` ]))
-                        // @TODO options for "where do I find x"
-                        .then(d => {
-                            d.close();
-                            return d;
-                        });
-                case 2:
-                    return d.player(DialogueEmote.DEFAULT, [ `No, I don't feel like it. Maybe later.` ])
-                        .then(d => d.npc(npc, DialogueEmote.DEFAULT, [ `Fine. I always knew you Adventurer types were callous`,
-                            `beasts. Go on your merry way!` ]))
-                        .then(d => {
-                            d.close();
-                            return d;
-                        });
-            }
-        });
+    ]).catch(() => {});
 };
 
 const talkToCookDuringQuestAction: npcAction = (details) => {
     const { player, npc } = details;
 
-    dialogueAction(player)
-        .then(d => d.npc(npc, DialogueEmote.DEFAULT, [ `Hey fam, how's the ingredient hunt going?` ]))
-        .then(d => d.close());
+    dialogue([ player, { npc, key: 'cook' }], [
+        npc => [ Emote.HAPPY, `Hey fam, how's the ingredient hunt going?` ]
+    ]);
 };
 
 export default new RunePlugin([{
---
+++ src/world/actor/dialogue.ts
@@ -0,0 +1,302 @@
+import { Npc } from '@server/world/actor/npc/npc';
+import { Player } from '@server/world/actor/player/player';
+import { Subscription } from 'rxjs';
+import { gameCache } from '@server/game-server';
+
+function parseDialogueFunctionArgs(func): string {
+    const str = func.toString();
+
+    if(!str) {
+        return null;
+    }
+
+    const argEndIndex = str.indexOf('=>');
+
+    if(argEndIndex === -1) {
+        return null;
+    }
+
+    let arg = str.substring(0, argEndIndex).trim();
+    if(!arg || arg.length === 0) {
+        return null;
+    }
+
+    return arg;
+}
+
+export enum Emote {
+    POMPOUS = 'POMPOUS',
+    UNKOWN_CREATURE = 'UNKOWN_CREATURE',
+    VERY_SAD = 'VERY_SAD',
+    HAPPY = 'HAPPY',
+    SHOCKED = 'SHOCKED',
+    WONDERING = 'WONDERING',
+    GOBLIN = 'GOBLIN',
+    TREE = 'TREE',
+    GENERIC = 'GENERIC',
+    SKEPTICAL = 'SKEPTICAL',
+    WORRIED = 'WORRIED',
+    DROWZY = 'DROWZY',
+    LAUGH = 'LAUGH',
+    SAD = 'SAD',
+    ANGRY = 'ANGRY',
+    EASTER_BUNNY = 'EASTER_BUNNY',
+
+    BLANK_STARE = 'BLANK_STARE',
+    SINGLE_WORD = 'SINGLE_WORD',
+    EVIL_STARE = 'EVIL_STARE',
+    LAUGH_EVIL = 'LAUGH_EVIL'
+}
+
+const nonLineEmotes = [ Emote.BLANK_STARE, Emote.SINGLE_WORD, Emote.EVIL_STARE, Emote.LAUGH_EVIL ];
+
+export enum EmoteAnimation {
+    POMPOUS_1LINE = 554,
+    POMPOUS_2LINE = 555,
+    POMPOUS_3LINE = 556,
+    POMPOUS_4LINE = 557,
+    UNKOWN_CREATURE_1LINE = 558,
+    UNKOWN_CREATURE_2LINE = 559,
+    UNKOWN_CREATURE_3LINE = 560,
+    UNKOWN_CREATURE_4LINE = 561,
+    VERY_SAD1LINE = 562,
+    VERY_SAD2LINE = 563,
+    VERY_SAD3LINE = 564,
+    VERY_SAD4LINE = 565,
+    SINGLE_WORD = 566,
+    HAPPY_1LINE = 567,
+    HAPPY_2LINE = 568,
+    HAPPY_3LINE = 569,
+    HAPPY_4LINE = 570,
+    SHOCKED_1LINE = 571,
+    SHOCKED_2LINE = 572,
+    SHOCKED_3LINE = 573,
+    SHOCKED_4LINE = 574,
+    WONDERING_1LINE = 575,
+    WONDERING_2LINE = 576,
+    WONDERING_3LINE = 577,
+    WONDERING_4LINE = 578,
+    BLANK_STARE = 579,
+    GOBLIN_1LINE = 580,
+    GOBLIN_2LINE = 581,
+    GOBLIN_3LINE = 582,
+    GOBLIN_4LINE = 583,
+    TREE_1LINE = 584,
+    TREE_2LINE = 585,
+    TREE_3LINE = 586,
+    TREE_4LINE = 587,
+    GENERIC_1LINE = 588,
+    GENERIC_2LINE = 589,
+    GENERIC_3LINE = 590,
+    GENERIC_4LINE = 591,
+    SKEPTICAL_1LINE = 592,
+    SKEPTICAL_2LINE = 593,
+    SKEPTICAL_3LINE = 594,
+    SKEPTICAL_4LINE = 595,
+    WORRIED_1LINE = 596,
+    WORRIED_2LINE = 597,
+    WORRIED_3LINE = 598,
+    WORRIED_4LINE = 599,
+    DROWZY_1LINE = 600,
+    DROWZY_2LINE = 601,
+    DROWZY_3LINE = 602,
+    DROWZY_4LINE = 603,
+    EVIL_STARE = 604,
+    LAUGH_1LINE = 605,
+    LAUGH_2LINE = 606,
+    LAUGH_3LINE = 607,
+    LAUGH_4LINE = 608,
+    LAUGH_EVIL = 609,
+    SAD_1LINE = 610,
+    SAD_2LINE = 611,
+    SAD_3LINE = 612,
+    SAD_4LINE = 613,
+    ANGRY_1LINE = 614,
+    ANGRY_2LINE = 615,
+    ANGRY_3LINE = 616,
+    ANGRY_4LINE = 617,
+    EASTER_BUNNY_1LINE = 1824,
+    EASTER_BUNNY_2LINE = 1825,
+    EASTER_BUNNY_3LINE = 1826,
+    EASTER_BUNNY_4LINE = 1827,
+}
+
+type DialogueTree = (Function | DialogueFunction)[];
+
+interface NpcParticipant {
+    npc: Npc | number;
+    key: string;
+}
+
+const playerWidgetIds = [ 64, 65, 66, 67 ];
+const npcWidgetIds = [ 241, 242, 243, 244 ];
+const optionWidgetIds = [ 228, 230, 232, 234 ];
+
+const charWidths = [ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 7, 14, 9, 12, 12, 4, 5,
+    5, 10, 8, 4, 8, 4, 7, 9, 7, 9, 8, 8, 8, 9, 7, 9, 9, 4, 5, 7,
+    9, 7, 9, 14, 9, 8, 8, 8, 7, 7, 9, 8, 6, 8, 8, 7, 10, 9, 9, 8,
+    9, 8, 8, 6, 9, 8, 10, 8, 8, 8, 6, 7, 6, 9, 10, 5, 8, 8, 7, 8,
+    8, 7, 8, 8, 4, 7, 7, 4, 10, 8, 8, 8, 8, 6, 8, 6, 8, 8, 9, 8,
+    8, 8, 6, 4, 6, 12, 3, 10, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+    4, 8, 11, 8, 8, 4, 8, 7, 12, 6, 7, 9, 5, 12, 5, 6, 10, 6, 6, 6,
+    8, 8, 4, 5, 5, 6, 7, 11, 11, 11, 9, 9, 9, 9, 9, 9, 9, 13, 8, 8,
+    8, 8, 8, 4, 4, 5, 4, 8, 9, 9, 9, 9, 9, 9, 8, 10, 9, 9, 9, 9,
+    8, 8, 8, 8, 8, 8, 8, 8, 8, 13, 6, 8, 8, 8, 8, 4, 4, 5, 4, 8,
+    8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 ];
+
+function wrapText(text: string, type: 'ACTOR' | 'TEXT'): string[] {
+    const maxWidth = type === 'ACTOR' ? 350 : 430;
+    const lines = [];
+
+    let lineStartIdx = 0;
+    let width = 0;
+    let lastSpace = 0;
+    let widthAfterSpace = 0;
+    for(let i = 0; i < text.length; i++) {
+        const charWidth = charWidths[text.charCodeAt(i)];
+        width += charWidth;
+        widthAfterSpace += charWidth;
+
+        if(text.charAt(i) === ' ' || text.charAt(i) === '\n') {
+            lastSpace = i;
+            widthAfterSpace = 0;
+        }
+
+        if(width >= maxWidth || text.charAt(i) === '\n') {
+            lines.push(text.substring(lineStartIdx, lastSpace));
+            lineStartIdx = lastSpace + 1;
+            width = widthAfterSpace;
+        }
+    }
+
+    if(lineStartIdx !== text.length - 1) {
+        lines.push(text.substring(lineStartIdx, text.length));
+    }
+
+    return lines;
+}
+
+export async function dialogue(participants: (Player | NpcParticipant)[], dialogueTree: DialogueTree): Promise<void> {
+    const player = participants.find(p => p instanceof Player) as Player;
+
+    if(!player) {
+        return Promise.reject('Player instance not provided to dialogue action.');
+    }
+
+    let stopLoop = false;
+
+    for(const dialogueAction of dialogueTree) {
+        if(stopLoop) {
+            return Promise.reject('Action cancelled.');
+        }
+
+        let sub: Subscription[] = [];
+
+        await new Promise((resolve, reject) => {
+            if(dialogueAction instanceof DialogueFunction) {
+                dialogueAction.execute();
+                resolve();
+                return;
+            }
+
+            const args = parseDialogueFunctionArgs(dialogueAction);
+            if(!args) {
+                console.error('No arguments passed to dialogue function.');
+                resolve();
+                return;
+            }
+
+            let widgetId: number;
+            let isOptions = false;
+
+            if(args === '()') {
+                const trees = dialogueAction();
+                const options = Object.keys(trees);
+                isOptions = true;
+                widgetId = optionWidgetIds[options.length - 2];
+
+                for(let i = 0; i < options.length; i++) {
+                    player.outgoingPackets.updateWidgetString(widgetId, 1 + i, options[i]);
+                }
+
+                sub.push(player.dialogueInteractionEvent.subscribe(choice => {
+                    sub.forEach(s => s.unsubscribe());
+                    const tree: DialogueTree = trees[options[choice - 1]];
+                    if(!tree || tree.length === 0) {
+                        resolve();
+                    } else {
+                        dialogue(participants, tree).then(() => resolve());
+                    }
+                }));
+            } else if(args === 'player') {
+                const dialogueDetails = dialogueAction(player);
+                const emote = dialogueDetails[0] as Emote;
+                const text = dialogueDetails[1] as string;
+                const lines = wrapText(text, 'ACTOR');
+                widgetId = playerWidgetIds[lines.length - 1];
+                const animation = nonLineEmotes.indexOf(emote) !== -1 ? EmoteAnimation[emote] : EmoteAnimation[`${emote}_${lines.length}LINE`];
+
+                player.outgoingPackets.setWidgetPlayerHead(widgetId, 0);
+                player.outgoingPackets.playWidgetAnimation(widgetId, 0, animation);
+                player.outgoingPackets.updateWidgetString(widgetId, 1, player.username);
+
+                for(let i = 0; i < lines.length; i++) {
+                    player.outgoingPackets.updateWidgetString(widgetId, 2 + i, lines[i]);
+                }
+            } else {
+                const participant = participants.find(p => (!(p instanceof Player) && p.key === args) ? p.npc : null) as NpcParticipant;
+                if(!participant || !participant.npc) {
+                    resolve();
+                    return;
+                }
+
+                let npc = participant.npc;
+                if(typeof npc !== 'number') {
+                    npc = npc.id;
+                }
+
+                const dialogueDetails = dialogueAction(npc);
+                const emote = dialogueDetails[0] as Emote;
+                const text = dialogueDetails[1] as string;
+                const lines = wrapText(text, 'ACTOR');
+                widgetId = npcWidgetIds[lines.length - 1];
+                const animation = nonLineEmotes.indexOf(emote) !== -1 ? EmoteAnimation[emote] : EmoteAnimation[`${emote}_${lines.length}LINE`];
+
+                player.outgoingPackets.setWidgetNpcHead(widgetId, 0, npc);
+                player.outgoingPackets.playWidgetAnimation(widgetId, 0, animation);
+                player.outgoingPackets.updateWidgetString(widgetId, 1, gameCache.npcDefinitions.get(npc).name);
+
+                for(let i = 0; i < lines.length; i++) {
+                    player.outgoingPackets.updateWidgetString(widgetId, 2 + i, lines[i]);
+                }
+            }
+
+            if(!isOptions) {
+                sub.push(player.dialogueInteractionEvent.subscribe(() => {
+                    sub.forEach(s => s.unsubscribe());
+                    resolve();
+                }));
+            }
+
+            player.activeWidget = {
+                widgetId: widgetId,
+                type: 'CHAT',
+                closeOnWalk: true,
+                forceClosed: () => reject('WIDGET_CLOSED')
+            };
+        }).then(() => {
+            sub.forEach(s => s.unsubscribe());
+        }).catch(() => {
+            sub.forEach(s => s.unsubscribe());
+            stopLoop = true;
+        });
+    }
+}
+
+class DialogueFunction {
+    constructor(public type: string, public execute: Function) {}
+}
+
+export const execute = (execute: Function): DialogueFunction => new DialogueFunction('execute', execute);
