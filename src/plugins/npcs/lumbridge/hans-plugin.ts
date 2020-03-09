b876da4c2b52040f1bc0a888e91b56b1202ac1b2
TheBlackParade <runejs.dev@gmail.com>
2020-03-08 19:03:58 -0500

    Testing Hans out.

--- src/plugins/npcs/lumbridge/hans-plugin.ts
+++ src/plugins/npcs/lumbridge/hans-plugin.ts
@@ -2,7 +2,7 @@ import { npcAction } from '@server/world/actor/player/action/npc-action';
 import { ActionType, RunePlugin } from '@server/plugins/plugin';
 import { npcIds } from '@server/world/config/npc-ids';
 import { animationIds } from '@server/world/config/animation-ids';
-import { dialogue, Emote, execute } from '@server/world/actor/dialogue';
+import { dialogue, Emote, execute, goto } from '@server/world/actor/dialogue';
 
 const action: npcAction = (details) => {
     const { player, npc } = details;
@@ -11,7 +11,7 @@ const action: npcAction = (details) => {
 
     dialogue([ player, { npc, key: 'hans' }], [
         hans => [ Emote.GENERIC, `Welcome to RuneScape!` ],
-        hans => [ Emote.HAPPY, `How do you feel about Rune.JS so far?\n` +
+        (hans, tag_Hans_Question) => [ Emote.HAPPY, `How do you feel about Rune.JS so far?\n` +
             `Please take a moment to let us know what you think!` ],
         options => ([
             `Love it!`, [
@@ -22,11 +22,6 @@ const action: npcAction = (details) => {
                 player => [ Emote.GENERIC, `It's kind of cool, I guess. Bit of a weird gimmick.` ],
                 hans => [ Emote.HAPPY, `Please let us know if you have any suggestions.` ]
             ],
-            `Eh, I don't know...`, [
-                player => [ Emote.SKEPTICAL, `Ehhh... I don't know...` ],
-                hans => [ Emote.GENERIC, `We're always open to feedback or Pull Requests anytime you like.` ],
-                player => [ Emote.GENERIC, `I'll keep that in mind, thanks.` ]
-            ],
             `Not my cup of tea, honestly.`, [
                 player => [ Emote.SKEPTICAL, `Not really my cup of tea, but keep at it.` ],
                 hans => [ Emote.GENERIC, `Thanks for the support!` ]
@@ -35,6 +30,10 @@ const action: npcAction = (details) => {
                 player => [ Emote.ANGRY, `Literally the worst thing I've ever seen. You disgust me on a personal level.` ],
                 hans => [ Emote.SAD, `I-is that so?... Well I'm... I'm sorry to hear that.` ],
                 execute(() => sadEnding = true)
+            ],
+            `What?`, [
+                player => [ Emote.DROWZY, `What?...` ],
+                goto('tag_Hans_Question')
             ]
         ]),
         execute(() => {
