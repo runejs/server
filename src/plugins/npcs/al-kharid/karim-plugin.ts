bfa2de1afb69ecdf8e66cf5935a50efef63fae56
Nickolaj Madsen <sqlbasedk@gmail.com>
2020-05-30 20:45:51 +0200

    Updated to the new dialogue system and removed the double qoutes and added spaces.

--- src/plugins/npcs/al-kharid/karim-plugin.ts
+++ src/plugins/npcs/al-kharid/karim-plugin.ts
@@ -1,31 +1,30 @@
-import {npcAction} from "@server/world/actor/player/action/npc-action";
-import {dialogueAction, DialogueEmote} from "@server/world/actor/player/action/dialogue-action";
-import {ActionType, RunePlugin} from "@server/plugins/plugin";
-import {npcIds} from "@server/world/config/npc-ids";
-import {itemIds} from "@server/world/config/item-ids";
-import {widgets} from "@server/world/config/widget";
+import { npcAction } from '@server/world/actor/player/action/npc-action';
+import { dialogueAction, DialogueEmote } from '@server/world/actor/player/action/dialogue-action';
+import { ActionType, RunePlugin } from '@server/plugins/plugin';
+import { npcIds } from '@server/world/config/npc-ids';
+import { itemIds } from '@server/world/config/item-ids';
+import { widgets } from '@server/world/config/widget';
+import { dialogue, Emote, execute, goto } from '@server/world/actor/dialogue';
 
 const talkToAction : npcAction = (details) => {
     const { player, npc } = details;
-    dialogueAction(player)
-        .then(d => d.npc(npc, DialogueEmote.JOYFUL, ['Would you like to buy a nice kebab? Only one gold.']))
-        .then(d => d.options('Would you like to buy a nice kebab? Only one gold.', ['I think I\'ll give it a miss.', 'Yes please.']))
-        .then(async d => {
-            switch (d.action) {
-                case 1:
-                    await d.player(DialogueEmote.CALM_TALK_1, ['I think i\'ll give it a miss.']);
-                    break;
 
-                case 2:
-                    await d.player(DialogueEmote.JOYFUL, ['Yes please.']);
+    dialogue([player, {npc, key: 'karim'}], [
+        karim => [ Emote.HAPPY, `Would you like to buy a nice kebab? Only one gold.`],
+        options => [
+            `I think i'll give it a miss.`, [
+                player => [Emote.DROWZY, `I think i'll give it a miss.`],
+            ],
+            `Yes please.`, [
+                player => [Emote.HAPPY, `Yes please.`],
+                execute(() => {
                     let inventory = player.inventory;
                     if (inventory.has(itemIds.coins)) {
                         let index = inventory.findIndex(itemIds.coins);
                         let item = inventory.items[index];
 
                         if (!inventory.hasSpace()) {
-                            await player.sendMessage(`You don't have enough space in your inventory.`);
-                            d.close();
+                            player.sendMessage(`You don't have enough space in your inventory.`);
                             return;
                         }
 
@@ -36,17 +35,19 @@ const talkToAction : npcAction = (details) => {
 
                         inventory.add({itemId: itemIds.kebab, amount: 1});
                         player.outgoingPackets.sendUpdateAllWidgetItems(widgets.inventory, inventory);
-                        d.close();
+                        return;
                     }
 
                     if (!inventory.has(itemIds.coins)) {
-                        d.player(DialogueEmote.ANNOYED, ['Oops, I forgot to bring any money with me.'])
-                            .then(d => d.npc(npc, DialogueEmote.CALM_TALK_1, ['Come back when you have some.']))
-                            .then(d => d.close());
+                        dialogue([player, {npc, key: 'karim'}], [
+                            (player, no_Money) => [Emote.ANGRY, `Oops, I forgot to bring any money with me.`],
+                            karim => [Emote.GENERIC, `Come back when you have some.`]
+                        ]);
                     }
-                    break;
-            }
-        });
+                })
+            ]
+        ]
+    ]);
 };
 
 export default new RunePlugin([
