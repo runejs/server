62d613326c83cc73c64e83b8d1b6100c8bf3e98a
Nickolaj Madsen <sqlbasedk@gmail.com>
2020-05-30 21:00:37 +0200

    Fixing the last few linting things.

--- src/plugins/npcs/al-kharid/karim-plugin.ts
+++ src/plugins/npcs/al-kharid/karim-plugin.ts
@@ -9,7 +9,7 @@ import { dialogue, Emote, execute, goto } from '@server/world/actor/dialogue';
 const talkToAction : npcAction = (details) => {
     const { player, npc } = details;
 
-    dialogue([player, {npc, key: 'karim'}], [
+    dialogue([player, { npc, key: 'karim' }], [
         karim => [ Emote.HAPPY, `Would you like to buy a nice kebab? Only one gold.`],
         options => [
             `I think i'll give it a miss.`, [
@@ -18,10 +18,10 @@ const talkToAction : npcAction = (details) => {
             `Yes please.`, [
                 player => [Emote.HAPPY, `Yes please.`],
                 execute(() => {
-                    let inventory = player.inventory;
+                    const inventory = player.inventory;
                     if (inventory.has(itemIds.coins)) {
-                        let index = inventory.findIndex(itemIds.coins);
-                        let item = inventory.items[index];
+                        const index = inventory.findIndex(itemIds.coins);
+                        const item = inventory.items[index];
 
                         if (!inventory.hasSpace()) {
                             player.sendMessage(`You don't have enough space in your inventory.`);
@@ -30,17 +30,17 @@ const talkToAction : npcAction = (details) => {
 
                         inventory.remove(index);
                         if (item.amount !== 1) {
-                            inventory.add({itemId: itemIds.coins, amount: item.amount-1});
+                            inventory.add( { itemId: itemIds.coins, amount: item.amount - 1 });
                         }
 
-                        inventory.add({itemId: itemIds.kebab, amount: 1});
+                        inventory.add({ itemId: itemIds.kebab, amount: 1 });
                         player.outgoingPackets.sendUpdateAllWidgetItems(widgets.inventory, inventory);
                         return;
                     }
 
                     if (!inventory.has(itemIds.coins)) {
-                        dialogue([player, {npc, key: 'karim'}], [
-                            (player, no_Money) => [Emote.ANGRY, `Oops, I forgot to bring any money with me.`],
+                        dialogue([player, { npc, key: 'karim' }], [
+                            player => [Emote.ANGRY, `Oops, I forgot to bring any money with me.`],
                             karim => [Emote.GENERIC, `Come back when you have some.`]
                         ]);
                     }
@@ -51,5 +51,5 @@ const talkToAction : npcAction = (details) => {
 };
 
 export default new RunePlugin([
-    { type: ActionType.NPC_ACTION, npcIds: npcIds.karim, options: 'talk-to', walkTo: true, action: talkToAction}
+    { type: ActionType.NPC_ACTION, npcIds: npcIds.karim, options: 'talk-to', walkTo: true, action: talkToAction }
 ]);
