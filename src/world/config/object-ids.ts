407071b5246f8dcd110f73f7a1cf77120ff67783
TheBlackParade <runejs.dev@gmail.com>
2020-06-05 19:35:15 -0500

    Adding support for server-side world-wide scenery spawns.

--- src/world/config/object-ids.ts
+++ src/world/config/object-ids.ts
@@ -10,5 +10,16 @@ export const objectIds = {
     ladders: {
         taverlyDungeonOverworld: 1759,
         taverlyDungeonUnderground: 1755,
-    }
+    },
+    brokenCart: 306,
+    brokenCartWheel: 327,
+    burntChest: 6420,
+    barricade: 4421,
+    crushedSkullBarricade: 6881,
+    fancyBarrel: 10293,
+    brokenDoor: 5439,
+    spearWall: 849,
+    crate: 357,
+    skeletonLayingFlat: 5358,
+    skeletonLayingAgainstWall: 5359,
 };
