975da25d496b02c402e5270b95c8732241cb9285
TheBlackParade <runejs.dev@gmail.com>
2020-06-06 18:30:20 -0500

    Fixing JS plugin hot reloading

--- plugins/combat/combat.js
+++ plugins/combat/combat.js
@@ -4,8 +4,9 @@ const action = async details => {
     const { player, npc } = details;
 
     npc.updateFlags.addDamage(1, DamageType.DAMAGE, 4, 5);
+    npc.say(`Ow!`);
 
-    await wait(1000);
+    await wait(100);
 
     player.updateFlags.addDamage(1, DamageType.DAMAGE, 9, 10);
 };
