40df17c6b7c57b0879d2fcd4abe41cdc760d63f6
Nickolaj Madsen <sqlbasedk@gmail.com>
2020-10-03 00:50:46 +0200

    Fixing adding spaces.. :D

--- src/plugins/commands/spawn-npc-command.ts
+++ src/plugins/commands/spawn-npc-command.ts
@@ -1,13 +1,12 @@
 import { ActionType, RunePlugin } from '@server/plugins/plugin';
 import { commandAction } from '@server/world/actor/player/action/input-command-action';
-import {cache, world} from "@server/game-server";
-import {Npc} from "@server/world/actor/npc/npc";
+import { cache, world } from "@server/game-server";
+import { Npc } from "@server/world/actor/npc/npc";
 
 const action: commandAction = (details) => {
     const {player, args} = details;
 
     const npcId: number = args.npcId as number;
-
     const npcDefinition = cache.npcDefinitions.get(npcId);
     const npc = new Npc({
         npcId: npcId,
