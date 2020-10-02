2c9c1deaef0d53fbfae46f32af4582c5e6914997
Nickolaj Madsen <sqlbasedk@gmail.com>
2020-10-03 00:45:53 +0200

    Added a command for spawning npcs.

---
+++ src/plugins/commands/spawn-npc-command.ts
@@ -0,0 +1,31 @@
+import { ActionType, RunePlugin } from '@server/plugins/plugin';
+import { commandAction } from '@server/world/actor/player/action/input-command-action';
+import {cache, world} from "@server/game-server";
+import {Npc} from "@server/world/actor/npc/npc";
+
+const action: commandAction = (details) => {
+    const {player, args} = details;
+
+    const npcId: number = args.npcId as number;
+
+    const npcDefinition = cache.npcDefinitions.get(npcId);
+    const npc = new Npc({
+        npcId: npcId,
+        x: player.position.x,
+        y: player.position.y
+    }, npcDefinition);
+
+    world.registerNpc(npc);
+};
+
+export default new RunePlugin({
+    type: ActionType.COMMAND,
+    commands: [ 'npc' ],
+    args: [
+        {
+            name: 'npcId',
+            type: 'number'
+        }
+    ],
+    action
+});
