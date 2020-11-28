e02a5db34638794073198f1ca697d352acaf07a7
Tynarus <github@tyn.dev>
2020-11-28 15:12:22 -0600

    Fixing lint warnings

--- src/plugins/commands/reset-camera-command.ts
+++ src/plugins/commands/reset-camera-command.ts
@@ -1,5 +1,7 @@
+import { PlayerCommandActionData } from '@server/world/action/player-command-action';
+
 export default {
     type: 'player_command',
     commands: [ 'reset_camera', 'resetcamera' ],
-    action: ({ player }) => player.outgoingPackets.resetCamera()
+    action: ({ player }: PlayerCommandActionData): void => player.outgoingPackets.resetCamera()
 };
