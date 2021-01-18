04d81c47bc6c5fd832f6d03a8f1b354d15dd8563
Harrison Cole <harrisoncole05@gmail.com>
2021-01-19 10:43:17 +1100

    - optimise Skills#getLevelForExp and Skills#getExpForLevel - TODO: unit testing.

--- src/world/actor/skills.ts
+++ src/world/actor/skills.ts
@@ -132,6 +132,17 @@ class SkillShortcuts implements SkillShortcutMap {
 
 export class Skills extends SkillShortcuts {
 
+    private static EXPERIENCE_LOOKUP_TABLE: number[] = [
+        0,83,174,276,388,512,650,801,969,1154,1358,1584,1833,2107,2411,2746,3115,3523,
+        3973,4470,5018,5624,6291,7028,7842,8740,9730,10824,12031,13363,14833,16456,18247,
+        20224,22406,24815,27473,30408,33648,37224,41171,45529,50339,55649,61512,67983,75127,
+        83014,91721,101333,111945,123660,136594,150872,166636,184040,203254,224466,247886,
+        273742,302288,333804,368599,407015,449428,496254,547953,605032,668051,737627,814445,
+        899257,992895,1096278,1210421,1336443,1475581,1629200,1798808,1986068,2192818,2421087,
+        2673114,2951373,3258594,3597792,3972294,4385776,4842295,5346332,5902831,6517253,7195629,
+        7944614,8771558,9684577,10692629,11805606,13034431
+    ];
+
     private _values: SkillValue[];
 
     public constructor(private actor: Actor, values?: SkillValue[]) {
@@ -177,28 +188,16 @@ export class Skills extends SkillShortcuts {
     }
 
     public getLevelForExp(exp: number): number {
-        let points = 0;
-        let output = 0;
-
-        for(let i = 1; i <= 99; i++) {
-            points += Math.floor(i + 300 * Math.pow(2, i / 7));
-            output = Math.floor(points / 4);
-            if(output >= exp) {
-                return i;
+        for (let level = 98; level >= 0; level--) {
+            if (Skills.EXPERIENCE_LOOKUP_TABLE[level] <= exp) {
+                return level + 1;
             }
         }
-
-        return 99;
+        return 0;
     }
 
     public getExpForLevel(level: number): number {
-        let xp: number = 0;
-
-        for(let i = 1; i < level; i++) {
-            xp += Math.floor(i + 300 * Math.pow(2, i / 7));
-        }
-
-        return Math.floor(xp / 4);
+        return Skills.EXPERIENCE_LOOKUP_TABLE[level];
     }
 
     public addExp(skill: number | SkillName, exp: number): void {
