8e258396489852aea0553e81670691410c9ae027
Nickolaj Madsen <sqlbasedk@gmail.com>
2021-02-22 20:11:57 +0100

    Added some changes.

--- src/plugins/skills/smithing/forging-plugin.ts
+++ src/plugins/skills/smithing/forging-plugin.ts
@@ -1,11 +1,57 @@
 import { itemOnObjectAction } from '@server/world/action/item-on-object-action';
 import { widgets } from '@server/config';
 import { Skill } from '@server/world/actor/skills';
-import { barIds, widgetItems } from '@server/plugins/skills/smithing/forging-constants';
+import { barIds, smithables, widgetItems } from '@server/plugins/skills/smithing/forging-constants';
 import { itemIds } from '@server/world/config/item-ids';
+import { cache } from '@server/game-server';
+import { Smithable } from '@server/plugins/skills/smithing/forging-types';
+import { itemAction } from '@server/world/action/item-action';
+
+const mapWidgetItemsToFlatArray = (input) => {
+    const result = [];
+    smithables.forEach((type) => {
+        type.forEach((smithable) => {
+            result.push(smithable.item.itemId);
+        });
+    });
+    return result;
+};
+
+const mapToFlatArray = (input) => {
+    const results = [];
+    input.forEach((values) => {
+        values.forEach((value) => {
+            results.push(value);
+        });
+    });
+    return results;
+};
+
+const findSmithableByItemId = (itemId) : Smithable => {
+    return mapToFlatArray(smithables).find((smithable) => {
+        return smithable.item.itemId === itemId;
+    });
+};
+
+const smithItem : itemAction = (details) => {
+    const { player, option, itemDetails } = details;
+    const smithable = findSmithableByItemId(itemDetails.id);
+    console.log('Option: ', option);
+    console.log('Item: ', itemDetails);
+    console.log('Smithable: ', smithable);
+
+    console.log(smithable.ingredient.amount + ` > ` + player.inventory.findAll(smithable.ingredient.itemId).length);
+    if (smithable.ingredient.amount > player.inventory.findAll(smithable.ingredient.itemId).length) {
+        player.interfaceState.closeAllSlots();
+        const bar = cache.itemDefinitions.get(smithable.ingredient.itemId);
+        player.sendMessage(`You don't have enough ${bar.name}s.`, true);
+        return;
+    }
+
+    player.playAnimation(898);
+};
 
 const openForgingInterface : itemOnObjectAction = (details) => {
-
     const { player, item } = details;
     const amountInInventory = player.inventory.findAll(item).length;
 
@@ -38,5 +84,10 @@ export default [
         objectIds: [2783],
         options: ['use'],
         action:  openForgingInterface
+    },
+    {
+        type: 'item_action',
+        itemIds: [...mapWidgetItemsToFlatArray(widgetItems)],
+        action: smithItem
     }
 ];
