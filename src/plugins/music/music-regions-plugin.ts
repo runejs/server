8c8525018875f8e7eb4ec0de101fab1d254c506b
Spencer Carlson <spencer.carlson@student.csulb.edu>
2021-03-05 14:55:58 -0800

    Remove: Unnecessary debug messages

--- src/plugins/music/music-regions-plugin.ts
+++ src/plugins/music/music-regions-plugin.ts
@@ -8,9 +8,7 @@ musicRegions.forEach(song => song.regionIds.forEach(region => musicRegionMap.set
 export function playSongForRegion(player: Player): void {
     const regionId = ((player.position.x >> 6) * 256) + (player.position.y >> 6);
     const songId: number = musicRegionMap.get(regionId);
-    logger.info('Song ID for this region: ' + songId);
-    logger.info('Size: ' + regionId);
-    player.sendMessage('Song playing: ' + getByValue(songs, songId));
+    logger.info('Playing: ' + getByValue(songs, songId) + ' (Song ID: ' + songId + ' Region ID: ' + regionId + ')');
 
     player.playSong(songId);
     player.metadata['updateMusic'] = false;
