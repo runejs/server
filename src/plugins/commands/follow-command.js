86fd151d97d336349f089d0130901dfbc11952df
Tynarus <github@tyn.dev>
2021-03-13 11:12:11 -0600

    Removing old testing command plugins

--- src/plugins/commands/follow-command.js
+++
@@ -1,7 +0,0 @@
-import { world } from '../../game-engine/game-server';
-
-module.exports = {
-    type: 'player_command',
-    commands: 'follow',
-    handler: details => details.player.follow(world.npcList[0])
-};
