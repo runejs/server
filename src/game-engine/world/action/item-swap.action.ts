1fd01ddfbea3f8dda6683924950df01872cf30ec
Tynarus <github@tyn.dev>
2021-05-01 11:13:21 -0500

    Fixing lint issues and finally removing old unused packages
    + updating some other packages and RxJS v7 final!

--- src/game-engine/world/action/item-swap.action.ts
+++ src/game-engine/world/action/item-swap.action.ts
@@ -44,7 +44,7 @@ export interface ItemSwapAction {
  * @param widget
  */
 const itemSwapActionPipe = (player: Player, fromSlot: number, toSlot: number,
-                            widget: { widgetId: number; containerId: number; }): RunnableHooks<ItemSwapAction> => {
+                            widget: { widgetId: number, containerId: number }): RunnableHooks<ItemSwapAction> => {
     const matchingHooks = getActionHooks<ItemSwapActionHook>('item_swap')
         .filter(plugin => numberHookFilter(plugin.widgetId || plugin.widgetIds, widget.widgetId));
 
