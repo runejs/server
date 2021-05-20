a224db84c74b3820cda5e598c886d31152e07e42
will <will_harris@unigroup.com>
2021-05-19 19:33:44 -0500

    added support for random food found inside game-world crates.

---
+++ src/plugins/objects/crates/crates.plugin.ts
@@ -0,0 +1,38 @@
+import { findItem, widgets } from '@engine/config';
+import { objectInteractionActionHandler } from '@engine/world/action/object-interaction.action';
+import { World } from '@engine/world';
+import { itemIds } from '@engine/world/config/item-ids';
+import { LandscapeObject } from '@runejs/filestore';
+
+export const action: objectInteractionActionHandler = (details) => {
+    details.player.busy = true;
+    details.player.playAnimation(827);
+    var random = Math.floor(Math.random() * 3);  
+    var veggies = [itemIds.onion, itemIds.grain, itemIds.cabbage];
+    var pickedItem = findItem(veggies[random]);
+    details.player.outgoingPackets.sendUpdateAllWidgetItems(widgets.inventory, details.player.inventory);
+
+    setTimeout(() => {
+        details.player.sendMessage(`You found a ${pickedItem.name.toLowerCase()} chest!.`);
+        details.player.playSound(2581, 7);
+        
+        details.player.instance.hideGameObjectTemporarily(details.object, 60);
+        
+
+        details.player.giveItem(pickedItem.gameId);
+        details.player.busy = false;
+    }, World.TICK_LENGTH);
+};
+
+export default {
+    pluginId: 'rs:crates',
+    hooks: [
+        {
+            type: 'object_interaction',
+            objectIds: [ 366, 357, 355 ],
+            options: ['loot', 'search', 'examine' ],
+            walkTo: true,
+            handler: action
+        }
+    ]
+};
