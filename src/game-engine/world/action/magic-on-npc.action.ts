9eb45e838886d7d8abba59a8003ac4c205b678fe
will <will_harris@unigroup.com>
2021-05-21 17:53:38 -0500

    Added magic attack packet handling, pipeline, actions, hooks, and finally plugins. Working full circle. No damage or effects calculated yet. No sounds, animations, or projectiles yet.

---
+++ src/game-engine/world/action/magic-on-npc.action.ts
@@ -0,0 +1,76 @@
+import { Player } from '@engine/world/actor/player/player';
+import { ActionHook, getActionHooks } from '@engine/world/action/hooks';
+import { advancedNumberHookFilter, questHookFilter } from '@engine/world/action/hooks/hook-filters';
+import { ActionPipe, RunnableHooks } from '@engine/world/action/index';
+import { Npc } from '../actor/npc/npc';
+
+
+/**
+ * Defines a button action hook.
+ */
+export interface Magic_on_NPCActionHook extends ActionHook<Magic_on_NPCAction, magic_on_npcActionHandler> {
+    // The npc world id that was clicked on after choosing the spell
+    npcworldId?: number;
+    // The IDs of the UI widgets that the buttons are on.
+    widgetIds?: number[];
+    // The child ID or list of child IDs of the button(s) within the UI widget.
+    buttonIds?: number | number[];
+    // Whether or not this item action should cancel other running or queued actions.
+    cancelActions?: boolean;
+}
+
+
+/**
+ * The button action hook handler function to be called when the hook's conditions are met.
+ */
+export type magic_on_npcActionHandler = (buttonAction: Magic_on_NPCAction) => void | Promise<void>;
+
+
+/**
+ * Details about a button action being performed.
+ */
+export interface Magic_on_NPCAction {
+    // The npc world id that was clicked on after choosing the spell
+    npc: Npc,
+    // The player performing the action.
+    player: Player;
+    // The ID of the UI widget that the button is on.
+    widgetId: number;
+    // The child ID of the button within the UI widget.
+    buttonId: number;
+}
+
+
+/**
+ * The pipe that the game engine hands button actions off to.
+ * @param npc
+ * @param player
+ * @param widgetId
+ * @param buttonId
+ */
+const buttonActionPipe = (npc:Npc, player: Player, widgetId: number, buttonId: number): RunnableHooks<Magic_on_NPCAction> => {
+    //console.info(`pew pew you use magic on ${npc.name}!`);
+
+    // Find all object action plugins that reference this location object
+    let matchingHooks = getActionHooks<Magic_on_NPCActionHook>('magic_on_npc');
+      
+
+    return {
+        hooks: matchingHooks,
+        actionPosition: player.position,
+        action: {
+            npc,
+            player,
+            widgetId,
+            buttonId
+        }
+    }
+    
+    
+};
+
+
+/**
+ * Button action pipe definition.
+ */
+export default [ 'magic_on_npc', buttonActionPipe ] as ActionPipe;
---
+++ src/plugins/buttons/magic-attack.plugin.ts
@@ -0,0 +1,46 @@
+
+import { Player } from '@engine/world/actor/player/player';
+import { Position } from '@engine/world/position';
+import { animationIds } from '@engine/world/config/animation-ids';
+import { soundIds } from '@engine/world/config/sound-ids';
+import { gfxIds } from '@engine/world/config/gfx-ids';
+import { loopingEvent } from '@engine/game-server';
+import { TaskExecutor } from '@engine/world/action';
+import { widgetButtonIds } from '../skills/smithing/smelting-constants';
+import { magic_on_npcActionHandler, Magic_on_NPCActionHook, Magic_on_NPCAction } from '../../game-engine/world/action/magic-on-npc.action';
+
+const buttonIds: number[] = [
+    0, // Home Teleport
+];
+
+function attack_target(player: Player, elapsedTicks: number): boolean {
+    console.log("attacking?");
+    return true;
+}
+
+const spells = ["Wind Strike","Confuse", "Water Strike","unknown?", "Earth Strike"];
+export const activate = (task: TaskExecutor<Magic_on_NPCAction>, elapsedTicks: number = 0) => {
+    const {
+        npc,
+        player,
+        widgetId,
+        buttonId
+    } = task.actionData;
+
+    console.info(`${player.username} smites ${npc.name} with ${spells[buttonId]}`);
+};
+
+export default {
+    pluginId: 'rs:magic',
+    hooks: 
+        {
+            type: 'magic_on_npc',
+            widgetId: 192,
+            buttonIds: buttonIds,
+            task: {
+                activate,
+                interval: 0
+            }
+        } as Magic_on_NPCActionHook
+    
+};
