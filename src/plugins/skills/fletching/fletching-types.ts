debdcc00508053d2dc84d60cf94b91d396e178df
Tynarus <github@tyn.dev>
2021-05-23 16:15:05 -0500

    Adding lint-fix command fixing existing code issues

--- src/plugins/skills/fletching/fletching-types.ts
+++ src/plugins/skills/fletching/fletching-types.ts
@@ -1,7 +1,7 @@
 import { Item }from '@engine/world/items/item';
 export interface Fletchable {
-  item: Item;
-  level: number;
-  experience: number;
-  ingredient: Item[];
+    item: Item;
+    level: number;
+    experience: number;
+    ingredient: Item[];
 }
