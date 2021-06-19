c12ddb44f599c90717205a000db0aade3afe950c
Tynarus <github@tyn.dev>
2021-06-19 13:02:27 -0500

    Adding support for changing room orientation when creating rooms
    The default orientation will now also be dependent upon which direction the room is being created in

--- src/plugins/skills/construction/room-builder.ts
+++ src/plugins/skills/construction/room-builder.ts
@@ -7,6 +7,44 @@ import { buttonActionHandler } from '@engine/world/action/button.action';
 import { getCurrentRoom } from '@plugins/skills/construction/util';
 import { Player } from '@engine/world/actor/player/player';
 import { Coords } from '@engine/world/position';
+import { dialogue, execute, goto } from '@engine/world/actor/dialogue';
+
+
+const newRoomOriention = (player: Player): number => {
+    const currentRoom = getCurrentRoom(player);
+
+    if(!currentRoom) {
+        return 0;
+    }
+
+    const playerLocalX = player.position.localX;
+    const playerLocalY = player.position.localY;
+
+    let deltaX = 0;
+    let deltaY = 0;
+
+    let orientation = 0;
+
+    if(playerLocalX === 7) {
+        // build east
+        deltaX = 1;
+        orientation = 1;
+    } else if(playerLocalX === 0) {
+        // build west
+        deltaX = -1;
+        orientation = 3;
+    } else if(playerLocalY === 7) {
+        // build north
+        deltaY = 1;
+        orientation = 0;
+    } else if(playerLocalY === 0) {
+        // build south
+        deltaY = -1;
+        orientation = 2;
+    }
+
+    return orientation;
+};
 
 
 export const canBuildNewRoom = (player: Player): Coords | null => {
@@ -65,7 +103,7 @@ export const canBuildNewRoom = (player: Player): Coords | null => {
 };
 
 
-export const roomBuilderWidgetHandler: buttonActionHandler = ({ player, buttonId }) => {
+export const roomBuilderWidgetHandler: buttonActionHandler = async ({ player, buttonId }) => {
     const newRoomCoords = canBuildNewRoom(player);
     if(!newRoomCoords) {
         return;
@@ -76,11 +114,34 @@ export const roomBuilderWidgetHandler: buttonActionHandler = ({ player, buttonId
         return;
     }
 
-    player.metadata.customMap.chunks[newRoomCoords.level][newRoomCoords.x][newRoomCoords.y] = new Room(chosenRoomType);
+    let createdRoom = new Room(chosenRoomType, newRoomOriention(player));
+    player.metadata.customMap.chunks[newRoomCoords.level][newRoomCoords.x][newRoomCoords.y] = createdRoom;
 
     player.interfaceState.closeAllSlots();
 
     openHouse(player);
+
+    await dialogue([ player ], [
+        (options, tag_Home) => [
+            'Rotate Counter-Clockwise', [
+                execute(() => {
+                    createdRoom.rotation = createdRoom.rotation > 0 ? createdRoom.rotation - 1 : 3;
+                    openHouse(player);
+                }),
+                goto('tag_Home')
+            ],
+            'Rotate Clockwise', [
+                execute(() => {
+                    createdRoom.rotation = createdRoom.rotation < 3 ? createdRoom.rotation + 1 : 0;
+                    openHouse(player);
+                }),
+                goto('tag_Home')
+            ],
+            'Accept', [
+                execute(() => {})
+            ]
+        ]
+    ]);
 };
 
 
