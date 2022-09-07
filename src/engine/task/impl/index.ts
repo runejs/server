f103d4a610271b4f605330b0a2abe4dcdb0c994c
James Monger <jameskmonger@hotmail.co.uk>
2022-09-07 15:08:52 +0100

    feat: create ActorLandscapeObjectInteractionTask
    fix: export ActorLandscapeObjectInteractionTask to index

--- src/engine/task/impl/index.ts
+++ src/engine/task/impl/index.ts
@@ -1,3 +1,4 @@
 export { ActorTask } from './actor-task'
 export { ActorWalkToTask } from './actor-walk-to-task'
 export { ActorWorldItemInteractionTask } from './actor-world-item-interaction-task'
+export { ActorLandscapeObjectInteractionTask } from './actor-landscape-object-interaction-task'
